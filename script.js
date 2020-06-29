10bfaad1422d76bcafc6b6265767dca704f41dad
Caleb Kirkish <caleb.kirkish@gmail.com>
2020-06-28 20:56:04 -0700

    finished the javascript section of the code!

--- script.js
+++ script.js
@@ -7,69 +7,68 @@ var num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var specialChars = '!@#$%^&*()'.split('');
 
 console.log("hello");
+
+var thePot = [];
+var result = "";
+
 // What options do you want for you password
 // Length, uppercase,lower,num, special
-function generatePassword() {
-    var passLength = prompt("How many characters would you like your password to be");
-    console.log("password length : ", passLength);
-    if (passLength < 8 || passLength > 128) {
-        alert("password length should be greater than 8 characters");
-        return
+
+var passLength = prompt("How many characters would you like your password to be");
+console.log("password length : " + passLength);
+if (passLength < 8 || passLength > 128) {
+    alert("password length should be greater than 8 characters");
+
+}
+
+else if (passLength >= 8 || passLength <= 128) {
+
+    // Boollean the users options
+    var confirmLowerCase = confirm("Press OK if you want lowercase letters");
+    console.log("lowercase : ", confirmLowerCase);
+    var confirmUpperCase = confirm("Press OK if you want uppercase letters");
+    console.log("uppercase : ", confirmUpperCase);
+    var confirmNumbers = confirm("Press OK if you want numbers");
+    console.log("numbers : ", confirmNumbers);
+    var confirmSpecialChars = confirm("Press OK if you want special characters");
+    console.log("special chars : ", confirmSpecialChars);
+
+
+
+
+
+    // What do we want in our pot of info
+    if (confirmLowerCase === true) {
+        thePot = thePot.concat(lowerCase);
+        console.log(thePot);
     }
+    if (confirmUpperCase === true) {
+        thePot = thePot.concat(upperCase);
+    }
+    if (confirmNumbers === true) {
+        thePot = thePot.concat(num);
+    }
+    if (confirmSpecialChars === true) {
+        thePot = thePot.concat(specialChars);
+        console.log(thePot);
+    }
+    if (thePot.length === 0) {
+        alert("Must select at least one option");
+    }
+    
+    else {
+        console.log(thePot.length);
+        console.log("i am in loop");
+        for (i = 0; i < passLength; i++) {
+          
+            var randomPot = thePot[Math.round(Math.random()*thePot.length)];    
+            result = result + randomPot;
 
-    else if (passLength >= 8 || passLength <= 128) {
-
-        // Boollean the users options
-        var confirmLowerCase = confirm("Press OK if you want lowercase letters");
-        console.log("lowercase : ", confirmLowerCase);
-        var confirmUpperCase = confirm("Press OK if you want uppercase letters");
-        console.log("uppercase : ", confirmUpperCase);
-        var confirmNumbers = confirm("Press OK if you want numbers");
-        console.log("numbers : ", confirmNumbers);
-        var confirmSpecialChars = confirm("Press OK if you want special characters");
-        console.log("special chars : ", confirmSpecialChars);
-
-        var userOptions = {
-            passLength: passLength,
-            hasLowerCase: confirmLowerCase,
-            hasUpperCase: confirmUpperCase,
-            hasNumbers: confirmNumbers,
-            hasSpecialChars: confirmSpecialChars,
-        }
-        return userOptions;
-
-
-        var options = getUserOptions();
-        var thePot = [];
-        var result = '';
-
-        // What do we want in our pot of info
-        if (options.hasLowerCase === true) {
-            thePot = thePot.concat(lowerCase);
-        }
-        if (options.hasUpperCase === true) {
-            thePot = thePot.concat(upperCase);
-        }
-        if (options.hasNumbers === true) {
-            thePot = thePot.concat(num);
-        }
-        if (options.hasSpecialChars === true) {
-            thePot = thePot.concat(specialChars);
-        }
-        if (options.userOptions === false) {
-            alert("Must select at least one option");
-        }
-        else {
-            for (i = 0; i < passLength; i++) {
-                var randomPot = thePot[Math.floor(math.random() * thePot.length)];
-                result += randomPot;
-            }
-        }
+     }
+     console.log(result);
     }
     console.log(result);
 }
+
+
     // Pick from the pot and make that the result    // Join result 
-
-
-
-
